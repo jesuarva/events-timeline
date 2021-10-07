21a24cd2646d5bda27f3357c4ff37df5db020230
@jesuarva <jesuarva.developer@gmail.com>
2021-10-07 18:31:38 +0100

    Define initial component

---
+++ src/components/EventsTimeLine/index.tsx
@@ -0,0 +1,87 @@
+import { FC } from "react";
+
+import "./EventsTimeLine.scss";
+
+const EventsTimeLine: FC = () => {
+  return (
+    <section className="timeline">
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title, title, title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe
+            atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
+            excepturi amet in dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <div className="time">12:30</div>
+          <h2>A long Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
+            nulla voluptatem recusandae dolor, nostrum excepturi amet in
+            dolores. Alias, ullam
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventsTimeLine;
