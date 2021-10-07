de947ca3081ece61e62f7404617ea7f9b803a9cb
@jesuarva <jesuarva.developer@gmail.com>
2021-10-07 20:37:04 +0100

    Update `EventsTimeLine` to render an array of `Events`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import EventsTimeLine from "./components/EventsTimeLine";
+import { useAppState } from "./store";
 
 function App() {
-  return <EventsTimeLine />;
+  const {
+    state: { events },
+    dispatch,
+  } = useAppState();
+
+  useEffect(() => {
+    const timer = setInterval(dispatch.updateEvents, 5_000);
+    return () => clearTimeout(timer);
+  }, [dispatch.updateEvents]);
+
+  return <EventsTimeLine events={events} />;
 }
 
 export default App;
--- src/components/EventsTimeLine/index.tsx
+++ src/components/EventsTimeLine/index.tsx
@@ -1,85 +1,33 @@
 import { FC } from "react";
+import { Event } from "../../types";
 
 import "./EventsTimeLine.scss";
 
-const EventsTimeLine: FC = () => {
+type Props = {
+  events: Event[] | [];
+};
+
+const EventsTimeLine: FC<Props> = ({ events }) => {
+  console.log("\x1b[45mJSAV\x1b[0m events  ", events);
   return (
     <section className="timeline">
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title, title, title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe
-            atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum
-            excepturi amet in dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="content">
-          <div className="time">12:30</div>
-          <h2>A long Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
-            ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed
-            nulla voluptatem recusandae dolor, nostrum excepturi amet in
-            dolores. Alias, ullam
-          </p>
-        </div>
-      </div>
+      {events
+        .sort((event1, event2) =>
+          Date.parse(event1.date) > Date.parse(event2.date) ? -1 : 1
+        )
+        .slice(0, 6)
+        .map(({ date, title, description }) => {
+          const time = date.match(/\d{2}:\d{2}/)?.[0];
+          return (
+            <div key={date} className="container">
+              <div className="content">
+                <div className="time">{`${time}`}</div>
+                <h2>{title}</h2>
+                <p>{description}</p>
+              </div>
+            </div>
+          );
+        })}
     </section>
   );
 };
